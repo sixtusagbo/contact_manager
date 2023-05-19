5acecfbfa5c6570e20921e6e82508bac33fbbb24
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-05-19 04:53:35 +0100

    Changing component state

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,27 +2,30 @@ import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 
 class Contact extends Component {
-  state = {};
-
-  onShowClick = (name, e) => {
-    console.log(name);
+  state = {
+    showContactInfo: false,
   };
 
   render() {
     const { name, email, phone } = this.props.contact;
+    const { showContactInfo } = this.state;
+
     return (
       <div className="card card-body mb-3">
         <h4>
           {name}{' '}
           <i
-            onClick={this.onShowClick.bind(this, name)}
+            onClick={() => this.setState({ showContactInfo: !showContactInfo })}
             className="fas fa-sort-down"
+            style={{ cursor: 'pointer' }}
           ></i>
         </h4>
-        <ul className="list-group">
-          <li className="list-group-item">Email: {email}</li>
-          <li className="list-group-item">Phone: {phone}</li>
-        </ul>
+        {showContactInfo ? (
+          <ul className="list-group">
+            <li className="list-group-item">Email: {email}</li>
+            <li className="list-group-item">Phone: {phone}</li>
+          </ul>
+        ) : null}
       </div>
     );
   }
