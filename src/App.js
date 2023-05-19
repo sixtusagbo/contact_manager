cb0bebecdfd4c875bec807cfc46039e6dd1cabc0
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-05-19 05:32:04 +0100

    Add Context API and Provider state w/ Consumer

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
+import Contacts from './components/Contacts';
 import Header from './components/Header';
+
+import { Provider } from './context';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import Contacts from './components/Contacts';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header brandName="Contact Manager" />
-        <div className="container">
-          <Contacts />
+      <Provider>
+        <div className="App">
+          <Header brandName="Contact Manager" />
+          <div className="container">
+            <Contacts />
+          </div>
         </div>
-      </div>
+      </Provider>
     );
   }
 }
