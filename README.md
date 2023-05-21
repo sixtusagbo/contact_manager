81869adba0fa87b647ac92fc23dc41b538fe33ec
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-05-21 07:39:10 +0100

    Update About page

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 [![pages-build-deployment](https://github.com/sixtusagbo/contact_manager/actions/workflows/pages/pages-build-deployment/badge.svg)](https://github.com/sixtusagbo/contact_manager/actions/workflows/pages/pages-build-deployment)
 
-I continue my react journey with this contact manager project. This is is just a basic functional reset, I'm gonna chnage my state management solution to Redux next, keep your eyes on the main branch for that update 😊.
+I continue my react journey with this contact manager project. With functional components and Redux as my state management solution, my app is as smooth as ever 😊.
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,11 +1,22 @@
 const About = () => {
   return (
     <div>
-      <h1 className="display-4">About Contact Manager</h1>
+      <h1 className="display-4 font-weight-bold">
+        About{' '}
+        <span className="text-warning font-weight-bolder">Contact Manager</span>
+      </h1>
       <p className="lead">
         Simple contact management app I made to learn React. This time, I
         migrated all my class based components to functional components - the
-        world of hooks 😊.
+        world of hooks. With{' '}
+        <a
+          href="https://redux.js.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Redux
+        </a>{' '}
+        as my state management solution my app runs smoothly 😊.
       </p>
       <p className="text-secondary">Version 2.0.0</p>
     </div>
